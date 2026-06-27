a85cbe115d2a2baae3412b20a1dc4c32ccd28577
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-06-26 22:37:29 -0400

    Init commit

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
