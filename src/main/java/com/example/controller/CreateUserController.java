f7f754f1e8ea184886af865c6f882dcc5ad8e1ef
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-06-30 23:37:57 -0400

    Added controller to the project

--- src/main/java/com/example/controller/CreateUserController.java
+++ src/main/java/com/example/controller/CreateUserController.java
@@ -1,5 +1,36 @@
 package com.example.controller;
 
-public class CreateUserController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dto.CreateUserRequest;
+import com.example.dto.UserResponse;
+import com.example.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api/users")
+public class CreateUserController {
+    private final UserService userService;
+
+    public CreateUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserResponse create(@RequestBody CreateUserRequest req) {
+        return userService.createUser(req);
+    }
+    
+    @GetMapping
+    public List<UserResponse> list() {
+        return userService.listUsers();
+    }
 }
