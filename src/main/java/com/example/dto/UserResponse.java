9e9283371b8783129d4642b24fd8e45c185c69d1
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-06-30 20:58:38 -0400

    Added dto, empty controller and empty service for create user

---
+++ src/main/java/com/example/dto/UserResponse.java
@@ -0,0 +1,35 @@
+package com.example.dto;
+
+import java.time.OffsetDateTime;
+
+public class UserResponse {
+    private Long id;
+    private String username;
+    private OffsetDateTime createdAt;
+
+    public UserResponse() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
