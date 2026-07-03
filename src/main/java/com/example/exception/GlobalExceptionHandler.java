60b9db1d73e843d5c7f76102417feb1301a2ecb6
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-02 22:35:10 -0400

    FIxed a bug of exception handler

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -6,11 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> handleDuplicatedUsername(UsernameAlreadyExistsException e) {
         return Map.of("message", e.getMessage());
