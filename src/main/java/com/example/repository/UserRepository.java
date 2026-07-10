9768e368bcf0fe5f7de3d96d82b5828d4aff3118
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-10 18:06:41 -0400

    Added query request on user repository to get user details

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
     public boolean existsByUsername(String username);
+    public Optional<User> findByUsername(String username);
 }
