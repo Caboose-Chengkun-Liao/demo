bc73a6c23013c41eef6af6d3907cfc005dd04e30
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-22 13:02:57 -0400

    Added jwt filter for request filtering

---
+++ src/main/java/com/example/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.example.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.service.JwtService;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.lang.Collections;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = extractToken(request);
+        
+        if(token != null) {
+            try {
+                String username = jwtService.validateAndGetUsername(token);
+
+                var auth = new UsernamePasswordAuthenticationToken(
+                    username, null, Collections.emptyList()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+
+            catch(JwtException e) {
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        if(request.getCookies() == null) return null;
+
+        for(Cookie c: request.getCookies()) {
+            if("token".equals(c.getName()))
+                return c.getValue();
+        }
+
+        return null;
+    }
+}
